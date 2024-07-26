52c9d06e97cf0bb6fc5ca48e2bfecd1a26fd43bb
DannyMarperOne <dannymarper.32@gmail.com>
2024-07-25 23:01:44 -0600

    Add feature 03 exercises

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -18,13 +18,51 @@ lineas de código.
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+let number = 28;
+let texto = "Ejemplo";
+let valor = true;
+let bigNumber = BigInt(989480202857333);
+// let bigNumber = 989480202857333n;
+let vacio = null;
+let noDefinido;
+let unico = Symbol("El número uno");
+
 // 4. Imprime por consola el valor de todas las variables
+console.log(number);
+console.log(texto);
+console.log(valor);
+console.log(bigNumber);
+console.log(vacio);
+console.log(noDefinido);
+console.log(unico);
 
 // 5. Imprime por consola el tipo de todas las variables
+console.log(number + " es de tipo: " + typeof number);
+console.log(texto + " es de tipo: " + typeof texto);
+console.log(valor + " es de tipo: " + typeof valor);
+console.log(bigNumber + " es de tipo: " + typeof bigNumber);
+console.log(vacio + " es de tipo: " + typeof vacio);
+console.log(noDefinido + " es de tipo: " + typeof noDefinido);
+console.log(typeof unico);
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
+number = 1024;
+texto = "Javascript";
+valor = false;
+bigNumber = BigInt(29292900000000000);
+//bigNumber = 0909884875590n;
+unico = Symbol("El unico");
+
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+number = "Cambio de dato";
+texto = 94;
+valor = 493893489324n;
+bigNumber = false;
+vacio = Symbol("Persona");
+noDefinido = null;
+unico;
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
