f7a3bb1a0d5104085cc8d1e08de9c5bc50562510
DannyMarperOne <dannymarper.32@gmail.com>
2024-08-07 15:56:59 -0600

    add line

--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -10,12 +10,13 @@ https://www.twitch.tv/videos/2206228701?t=00h16m02s
 
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
 
+
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
 
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
 
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
-//    dependiendo de la edad 
+//    dependiendo de la edad
 
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
 
