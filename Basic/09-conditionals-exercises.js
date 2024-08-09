536577923b6d4e690cbf527cc4407bff4f5754a1
DannyMarperOne <dannymarper.32@gmail.com>
2024-08-08 23:27:39 -0600

    end conditionals-exesrcises

--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -54,8 +54,78 @@ let person = 80;
 console.log(person >= 18 ? "Adulto" : "Menor");
 
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
+let meses = 11;
+if (meses === 1) {
+    console.log("Enero");
+} else if (meses === 2) {
+    console.log("Febrero");
+} else if (meses === 3) {
+    console.log("Marzo");
+} else if (meses === 4) {
+    console.log("Abril");
+} else if (meses === 5) {
+    console.log("Mayo");
+} else if (meses === 6) {
+    console.log("Junio");
+} else if (meses === 7) {
+    console.log("Julio");
+} else if (meses === 8) {
+    console.log("Agosto");
+} else if (meses === 9) {
+    console.log("Septiembre");
+} else if (meses === 10) {
+    console.log("Octubre");
+} else if (meses === 11) {
+    console.log("Noviembre");
+} else if (meses === 12) {
+    console.log("Diciembre");
+} else {
+    console.log("Mes incorrecto");
+}
 
-let mounth = 10,
+// 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
+let arrMes = [0, 2, 4, 6, 7, 9, 11];
+if (meses === 1) {
+    console.log(`${meses} tiene 28 o 29 días`);
+} else if (arrMes.includes(meses)) {
+    console.log(`${meses} tiene 31 días`);
+} else {
+    console.log(`${meses} tiene 30 días`);
+}
+
+
+/*
+Mi versión larga
+
+if (mounth === 1) {
+    console.log(`${nowMounth} tiene 28 o 29 días`);
+} else if (mounth === 0 || mounth === 2 || mounth === 4 || mounth === 6 || mounth === 7 || mounth === 9 || mounth === 11) {
+    console.log(`${nowMounth} tiene 31 días`);
+} else {
+    console.log(`${nowMounth} tiene 30 días`);
+} */
+
+
+// switch
+// 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+let languaje = "Latin";
+switch (languaje) {
+    case "Japones": console.log("Kon'nichiwa, hajimemashite");
+        break;
+    case "Espanol": console.log("Hola, mucho gusto");
+        break;
+    case "Ingles": console.log("Hi, nice to meet you");
+        break;
+    case "Ruso": console.log("Privet, priyatno poznakomit'sya");
+        break;
+    case "Latin": console.log("Hi delicatus in occursum adventus tui");
+        break;
+    default: console.log("No elegiste ningun idioma disponible");
+        break;
+}
+
+// 9. Usa un switch para hacer de nuevo el ejercicio 6
+let mounth = 9,
     nowMounth;
 
 switch (mounth) {
@@ -100,33 +170,34 @@ switch (mounth) {
         break;
 }
 console.log(nowMounth);
-
-// 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
-let arrMes = [0, 2, 4, 6, 7, 9, 11];
-if (mounth === 1) {
-    console.log(`${nowMounth} tiene 28 o 29 días`);
-} else if (arrMes.includes(mounth)) {
-    console.log(`${nowMounth} tiene 31 días`);
-} else {
-    console.log(`${nowMounth} tiene 30 días`);
-}
-
-
-/*
-Mi versión larga
-
-if (mounth === 1) {
-    console.log(`${nowMounth} tiene 28 o 29 días`);
-} else if (mounth === 0 || mounth === 2 || mounth === 4 || mounth === 6 || mounth === 7 || mounth === 9 || mounth === 11) {
-    console.log(`${nowMounth} tiene 31 días`);
-} else {
-    console.log(`${nowMounth} tiene 30 días`);
-} */
-
-
-// switch
-// 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
-
-// 9. Usa un switch para hacer de nuevo el ejercicio 6
-
 // 10. Usa un switch para hacer de nuevo el ejercicio 7
+// 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
+
+switch (nowMounth) {
+    case "Enero": console.log(`El mes de ${nowMounth} tiene 31 días`);
+        break;
+    case "Febrero": console.log(`El mes de ${nowMounth} tiene 28 o 29 días`);
+        break;
+    case "Marzo": console.log(`El mes de ${nowMounth} tiene 31 días`);
+        break;
+    case "Abril": console.log(`El mes de ${nowMounth} tiene 30 días`);
+        break;
+    case "Mayo": console.log(`El mes de ${nowMounth} tiene 31 días`);
+        break;
+    case "Junio": console.log(`El mes de ${nowMounth} tiene 30 días`);
+        break;
+    case "Julio": console.log(`El mes de ${nowMounth} tiene 31 días`);
+        break;
+    case "Agosto": console.log(`El mes de ${nowMounth} tiene 31 días`);
+        break;
+    case "Septiembre": console.log(`El mes de ${nowMounth} tiene 30 días`);
+        break;
+    case "Octubre": console.log(`El mes de ${nowMounth} tiene 31 días`);
+        break;
+    case "Noviembre": console.log(`El mes de ${nowMounth} tiene 30 días`);
+        break;
+    case "Diciembre": console.log(`El mes de ${nowMounth} tiene 31 días`);
+        break;
+    default: console.log("Error en el programa");
+        break;
+}
